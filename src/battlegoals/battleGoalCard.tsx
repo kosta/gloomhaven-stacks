e5eb354eeb8330fc1f0dfdd5aa7cbe9313d4c5c8
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-02-19 22:28:03 +0100

    build battle goals with css

--- src/battlegoals/battleGoalCard.tsx
+++ src/battlegoals/battleGoalCard.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { noop } from "lang/react";
 import { battleGoalByLocalId, battleGoalImages } from "battlegoals/battleGoals";
+import { range } from "../lang/ranges";
 
 interface BattleGoalCardProps {
   battleGoalId: number
@@ -27,18 +28,90 @@ export default class BattleGoalCard extends React.Component<BattleGoalCardProps,
   }
 
   public render(): React.ReactNode {
-    const imageStyle = {
+    const battleGoalStyle = {
+      position: 'relative',
       height: 300,
       width: 200
     } as React.CSSProperties;
-    const imageUrl = this.state.hidden ?
-      battleGoalImages.background :
-      "https://raw.githubusercontent.com/any2cards/gloomhaven/master/images/battle-goals/" + (battleGoalByLocalId(this.props.battleGoalId).name) + ".png";
 
+    const imageStyle = {
+      height: '100%',
+      width: '100%'
+    };
 
+    const imageUrl = this.state.hidden ? battleGoalImages.background : battleGoalImages.foreground;
 
-    return <div onMouseOver={this.reveal} onMouseOut={this.hide}>
-                <img src={imageUrl} style={imageStyle}/>
+    // "https://raw.githubusercontent.com/any2cards/gloomhaven/master/images/battle-goals/" + (battleGoalByLocalId(this.props.battleGoalId).name) + ".png"
+
+    return <div style={battleGoalStyle} onMouseOver={this.reveal} onMouseOut={this.hide}>
+      <img style={imageStyle} src={imageUrl} alt='ups'/>
+      {this.renderOverlay()}
+    </div>;
+  }
+
+  private renderOverlay(): React.ReactNode {
+    if (this.state.hidden) {
+      return null;
+    }
+
+    const overlayStyle = {
+      position: 'absolute',
+      top: '22%',
+      right: '20%',
+      bottom: '10%',
+      left: '20%',
+      textAlign: 'center'
+    } as React.CSSProperties;
+
+    const textStyle = {
+      position: 'absolute',
+      width: '100%'
+    } as React.CSSProperties;
+
+    const titleStyle = {
+      margin: '0 0 17% 0',
+      fontSize: '1.35em',
+      fontWeight: 'normal'
+    } as React.CSSProperties;
+
+    const descriptionStyle = {
+      position: 'absolute',
+      fontSize: '0.8em',
+      lineHeight: '1.2em',
+      left: '50%',
+      width: '68%',
+      margin: ' 0 0 0 -34%'
+    } as React.CSSProperties;
+
+    const perksStyle = {
+      position: 'absolute',
+      bottom: '12.5%',
+      left: '50%',
+      width: '50%',
+      margin: '0 0 0 -30%',
+      fontSize: '2em',
+      fontWeight: 'bold'
+    } as React.CSSProperties;
+
+    const cardNumberStyle = {
+      position: 'absolute',
+      bottom: '3%',
+      left: '50%',
+      width: '20%',
+      margin: '0 0 0 -15%',
+      color: 'white',
+      fontSize: 'xx-small',
+    } as React.CSSProperties;
+
+    const battleGoal = battleGoalByLocalId(this.props.battleGoalId);
+
+    return <div style={overlayStyle}>
+      <div style={textStyle}>
+        <h3 style={titleStyle}>{battleGoal.displayName}</h3>
+        <section style={descriptionStyle}>{battleGoal.text}</section>
+      </div>
+      <div style={perksStyle}>{range(0, battleGoal.reward).map(() => '✓').join('')}</div>
+      <div style={cardNumberStyle}>{battleGoal.globalCardId}</div>
     </div>;
   }
 }
