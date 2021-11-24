91683514432a1f19f36c91ef9ef73d9b248d35c6
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-11-24 23:53:34 +0100

    migrate tests to testing library

--- test/app/App.spec.tsx
+++ test/app/App.spec.tsx
@@ -1,15 +1,16 @@
-import * as React from 'react'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { App } from 'app/App';
-import { CardStack } from 'cards/cards';
 import { CardStacks } from 'app/GloomHaven';
-import { shallow } from 'enzyme';
+import { CardStack } from 'cards/cards';
+import * as React from 'react';
 
 const emptyCardStack = (): CardStack => {
   return {
     list: [],
     stack: [],
     history: []
-  }
+  };
 };
 
 describe('App', () => {
@@ -25,39 +26,67 @@ describe('App', () => {
         randomScenarios: emptyCardStack(),
         singleItems: emptyCardStack(),
         personalGoals: emptyCardStack(),
-        prosperity: 1,
+        prosperity: 1
       };
     });
 
-    function appComponentInstance() {
-      const wrapper = shallow(<App initialCardStacks={initialCardStacks}/>);
-      return wrapper.instance() as App;
-    }
+    const appComponentInstance = () => {
+      render(<App initialCardStacks={initialCardStacks}/>);
+    };
 
-    it('add previously not contained card', () => {
-      const app = appComponentInstance();
-      app.addCards('Item Designs', [1]);
-      expect(app.state.stacks.itemDesigns.list).toContain(1);
+    it('add previously not contained card', async () => {
+      appComponentInstance();
+      await addCards('item designs', 1);
+      const cardStack = await exportedStacks();
+      expect(cardStack.itemDesigns.list).toContain(1)
     });
 
-    it('add the same card only once', () => {
-      const app = appComponentInstance();
-      app.addCards('City Events', [1, 1]);
-      expect(app.state.stacks.cityEvents.stack).toEqual([1]);
+    it('add the same card only once', async () => {
+      appComponentInstance();
+      await addCards('City Events', 1, 1)
+      const cardStack = await exportedStacks();
+      expect(cardStack.cityEvents.stack).toEqual([1]);
     });
 
-    it('filter out already contained card', () => {
-      const app = appComponentInstance();
-      app.addCards('City Events', [1]);
-      app.addCards('City Events', [1, 2]);
-      expect(app.state.stacks.cityEvents.stack).toContain(2);
+    it('filter out already contained card', async  () => {
+      appComponentInstance();
+      await addCards('City Events', 1)
+      await addCards('City Events', 1, 2)
+      const cardStack = await exportedStacks();
+      expect(cardStack.cityEvents.stack.length).toBe(2);
+      expect(cardStack.cityEvents.stack).toContain(1);
+      expect(cardStack.cityEvents.stack).toContain(2);
     });
 
-    it('do not add a already contained card', () => {
-      const app = appComponentInstance();
-      app.addCards('City Events', [1]);
-      app.addCards('City Events', [1]);
-      expect(app.state.stacks.cityEvents.stack).toEqual([1]);
+    it('do not add a already contained card', async () => {
+      appComponentInstance();
+      await addCards('City Events', 1);
+      await addCards('City Events', 1);
+      const cardStack = await exportedStacks();
+      expect(cardStack.cityEvents.stack).toEqual([1]);
     });
+
   });
 });
+
+const addCards = async (cardType: string, ... cardIds: number[]) => {
+  const cardRegex = new RegExp(cardType, 'i')
+  const cardIdsAsString = cardIds.join(' ');
+  userEvent.click(await screen.findByRole('button', { name: 'Add Cards' }));
+  const itemDesignsInput = await screen.findByRole('textbox', {
+    name: cardRegex
+  });
+  userEvent.type(itemDesignsInput, cardIdsAsString);
+  const itemDesignsAddButton = await screen.findByRole('button', {
+    name: cardRegex
+  });
+  userEvent.click(itemDesignsAddButton);
+};
+
+const exportedStacks = async () => {
+  userEvent.click(await screen.findByRole('button', {
+    name: /import \/ export/i
+  }));
+  const textElement = await screen.findByRole('textbox') as HTMLTextAreaElement;
+  return JSON.parse(textElement.value) as CardStacks;
+};
