8124f0686e0d8dc27fdd4741922f73be9abfb24d
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-01-12 23:44:21 +0100

    update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom';
-import { App } from "app/App";
-import "./style.css";
+import { App } from 'app/App';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
 
 const initialCardStacks = App.initializeStacksFromLocalStorage();
-let app = <App initialCardStacks={initialCardStacks}/>;
-ReactDOM.render(
-  app,
-  document.getElementById('root')
-);
+const app = <App initialCardStacks={initialCardStacks}/>;
+
+const container = document.getElementById('root');
+if (container === null) {
+  throw new Error('root element not found')
+}
+createRoot(container).render(app);
