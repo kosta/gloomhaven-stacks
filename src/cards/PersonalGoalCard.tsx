cf93b33ee4d6afa92c2d366178a37be930220eca
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-01-15 21:50:03 +0100

    convert PersonalGoalCard into function component

--- src/cards/PersonalGoalCard.tsx
+++ src/cards/PersonalGoalCard.tsx
@@ -1,19 +1,18 @@
 import * as React from 'react'
-import { css, NoState } from '../lang/react'
+import { css } from '../lang/react'
 
 interface PersonalGoalCardProps {
   cardId: number
 }
 
-export class PersonalGoalCard extends React.Component<PersonalGoalCardProps, NoState> {
-  render() {
-    const style = css({
-      display: 'inline-block',
-      width: '605px',
-      borderRadius: '20px',
-    })
-    const cardId = this.props.cardId
-    const imgSrc = `https://raw.githubusercontent.com/any2cards/worldhaven/master/images/personal-quests/gloomhaven/gh-pq-${cardId}.png`
-    return <img key={cardId} style={style} src={imgSrc} alt="personal goal" />
-  }
+const style = css({
+  display: 'inline-block',
+  width: '605px',
+  borderRadius: '20px',
+})
+
+export const PersonalGoalCard = (props: PersonalGoalCardProps) => {
+  const cardId = props.cardId
+  const imgSrc = `https://raw.githubusercontent.com/any2cards/worldhaven/master/images/personal-quests/gloomhaven/gh-pq-${cardId}.png`
+  return <img key={cardId} style={style} src={imgSrc} alt="personal goal" />
 }
